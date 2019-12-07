5d498554823430b0d7a9a76c94d9f585db692c4b
Emil Burzo <contact@emilburzo.com>
2019-12-07 13:20:36 +0200

    fix default ingest date

--- src/main/kotlin/com/emilburzo/db/models.kt
+++ src/main/kotlin/com/emilburzo/db/models.kt
@@ -2,7 +2,7 @@ package com.emilburzo.db
 
 import org.jetbrains.exposed.dao.LongIdTable
 import org.jetbrains.exposed.sql.datetime
-import java.time.LocalDateTime
+import org.joda.time.DateTime
 
 /**
  * Created by emil on 07.12.2019.
@@ -10,7 +10,7 @@ import java.time.LocalDateTime
 object DbNews : LongIdTable(name = "news") {
     val sourceName = varchar(name = "source", length = 100)
     val publishDate = datetime(name = "publish_date").index()
-    val ingestDate = datetime(name = "ingest_date").clientDefault { LocalDateTime.now() }
+    val ingestDate = datetime(name = "ingest_date").clientDefault { DateTime() }
     val title = varchar(name = "title", length = 512)
     val url = varchar(name = "url", length = 512).uniqueIndex()
 }
