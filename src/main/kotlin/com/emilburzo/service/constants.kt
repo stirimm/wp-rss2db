f316c1774073d20c1dc97ba787043cd64dd61d69
Emil Burzo <contact@emilburzo.com>
2019-12-07 13:08:49 +0200

    use https for all RSS feeds

--- src/main/kotlin/com/emilburzo/service/constants.kt
+++ src/main/kotlin/com/emilburzo/service/constants.kt
@@ -5,9 +5,9 @@ package com.emilburzo.service
  */
 val NEWS_RSS_URLS = setOf(
     NewsRssUrl("emaramures", "https://www.emaramures.ro/feed/"),
-    NewsRssUrl("jurnalmm", "http://jurnalmm.ro/feed/"),
+    NewsRssUrl("jurnalmm", "https://jurnalmm.ro/feed/"),
     NewsRssUrl("vasiledale", "https://vasiledale.ro/feed/"),
     NewsRssUrl("ziarmm", "https://ziarmm.ro/feed/"),
-    NewsRssUrl("actualmm", "http://www.actualmm.ro/feed/"),
+    NewsRssUrl("actualmm", "https://www.actualmm.ro/feed/"),
     NewsRssUrl("directmm", "https://www.directmm.ro/feed/")
 )
