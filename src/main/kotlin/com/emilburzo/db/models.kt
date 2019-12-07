127ceb9c216d64b8e6b469138c3416429d1bb6e6
Emil Burzo <contact@emilburzo.com>
2019-12-07 13:50:50 +0200

    add comment about timezones

--- src/main/kotlin/com/emilburzo/db/models.kt
+++ src/main/kotlin/com/emilburzo/db/models.kt
@@ -9,6 +9,8 @@ import org.joda.time.DateTime
  */
 object DbNews : LongIdTable(name = "news") {
     val sourceName = varchar(name = "source", length = 100)
+    // timezone is lost at the moment - https://github.com/JetBrains/Exposed/issues/221
+    // but since all Wordpress RSS feeds are in UTC, that's ok as they are comparable
     val publishDate = datetime(name = "publish_date").index()
     val ingestDate = datetime(name = "ingest_date").clientDefault { DateTime() }
     val title = varchar(name = "title", length = 512)
